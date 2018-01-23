e8f0cb2b2ca4390a3f22496e103c30a852b67dfa
David Aktary <david@aktary.com>
2018-01-23 13:35:26 -0600

    Update the copy on the docs homepage.

--- docs/src/app/home.tsx
+++ docs/src/app/home.tsx
@@ -20,8 +20,13 @@ export class Home extends React.Component<IHomeProps> {
               <h1>A protocol and tool set for sharing liquidity</h1>
               <p>
                 ERC dEX believes that the future of decentralized exchange depends on deep liquidity pools and robust relayer networks.
-                We've developed a protocol and tools to make integration easy and simple.
+                We've developed Aqueduct to be an <a href='https://github.com/ercdex/aqueduct'>open source</a> protocol and tool set to make integration and liquidity sharing simple.
               </p>
+              <p>If you would like to apply for fee sharing or fee rebates, please <a href='mailto:partners@ercdex.com'>email us</a> with details on your intended usage.</p>
+              <h2>Installation</h2>
+              <code>npm install aqueduct --save</code>
+              <h2>Examples</h2>
+              <p>You can find example implementations in the <code>examples</code> directory of <a href='https://github.com/ercdex/aqueduct'>the Aqueduct Github repository</a></p>
               <div className='fl sb button-bar'>
                 <Link to={getPath(paths.rest)} className='link'>REST API</Link>
                 <Link to={getPath(paths.client)} className='link'>JavaScript Client</Link>
