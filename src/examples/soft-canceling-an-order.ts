21c5416efaf433b719139d303bea70ee6c8d965a
Luke Autry <lukeautry@gmail.com>
2018-05-01 14:19:53 -0400

    1.1.21

--- src/examples/soft-canceling-an-order.ts
+++ src/examples/soft-canceling-an-order.ts
@@ -2,6 +2,8 @@ import { Aqueduct } from '../generated/aqueduct';
 import { SoftCancelOrder } from '../soft-cancel-order';
 
 (async () => {
+  Aqueduct.Initialize();
+
   await new SoftCancelOrder({
     orderHash: '0xc5d2902a1271efc05f947a80bdab4948b13d9465146dff5de038afc10bdca6b1',
     nodeUrl: 'http://localhost:8545'
