17a45a50cdb5177ac5dbfe52953be724f221c7c3
Luke Autry <lukeautry@gmail.com>
2018-05-01 14:22:29 -0400

    1.1.22

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { FillOrder } from './fill-order';
 import { Aqueduct } from './generated/aqueduct';
 import { LimitOrder } from './limit-order';
 import { MarketOrder } from './market-order';
+import { SoftCancelOrder } from './soft-cancel-order';
 
 export {
   Aqueduct,
   LimitOrder,
   CancelOrder,
   FillOrder,
-  MarketOrder
+  MarketOrder,
+  SoftCancelOrder
 };
